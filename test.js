a46789953635204a8e791d8fd0bce30b6fe6ce71
Thomas Hooper <tom@stainlessvision.com>
2015-03-14 18:07:54 +0000

    squaresine

--- test.js
+++ test.js
@@ -5,17 +5,17 @@
 
 import { Multiwave } from './index';
 
-function squareSine(pow, length) {
-  pow += 1;
+function squareSine(periodIndex, length) {
+  var pow = (periodIndex * 2) + 4;
   return function(x) {
-    x /= length;
-    return x;
-    return Math.pow(period - 3, pow) - 1;
+    // x /= length;
+    // return x;
+    // return Math.pow(x - 3, pow);
     
-    var period = x / (Math.PI / 2) % 4;
+    var period = x / (length / 4);
     var a = Math.pow(period - 3, pow) - 1;
     var b = -Math.pow(period - 1, pow) + 1;
-    return period > 2 ? a : b;
+    return (period > 2 ? a : b);
   };
 }
 
@@ -29,5 +29,5 @@ var wave = Multiwave(squareSine);
 
 export function dsp(t){
   // return 0;
-  return wave(43);
+  return wave(43*2);
 }
